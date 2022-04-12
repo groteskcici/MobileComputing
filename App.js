8762eadfadbef79c6d646ff7f0b20eadb2989a36
Christopher Luzzi <christopher.luzzi@student.reutlingen-university.de>
2022-04-12 21:43:45 +0200

    done

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { View, Image, StyleSheet } from 'react-native-web';
 import {db} from './firebase-config';
 import {addDoc, collection, getDocs} from "firebase/firestore"
 import { async } from '@firebase/util';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default function App() {
@@ -19,6 +17,9 @@ export default function App() {
       await addDoc(usersCollectionRef, {name: newName, email: newEmail, url: newUrl});
   };
 
+
+
+
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
@@ -28,38 +29,25 @@ export default function App() {
     getUsers();
   }, []);
 
-
-  const Stack = createNativeStackNavigator();
-
   return (
-    <NavigationContainer>
-        <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: "Welcome"}}
-        />
-        <div className='App'> 
-            <input placeholder='Name...'  onChange={(event) => {setNewName(event.target.value);}}/>
-            <input placeholder='Email' onChange={(event) => {setNewEmail(event.target.value);}}/>
-            <input placeholder='URL' onChange={(event) => {setNewUrl(event.target.value);}}/>
-            <button onClick={createUser}>Create User</button>
-              {users.map((user) => {
-                return (
-                  <div>
-                    <h1>Name: {user.name}</h1>          
-                    <h1>E-Mail: {user.email}</h1>
-                    <Image
-                        style={{width: 50, height: 50}}
-                        source={{uri : user.url}}
-                    />
-                  </div>
-                  );
-              })}  
-            </div>
-            </Stack.Navigator>
-    </NavigationContainer>
-
+    <div className='App'> 
+    <input placeholder='Name...'  onChange={(event) => {setNewName(event.target.value);}}/>
+    <input placeholder='Email' onChange={(event) => {setNewEmail(event.target.value);}}/>
+    <input placeholder='URL' onChange={(event) => {setNewUrl(event.target.value);}}/>
+    <button onClick={createUser}>Create User</button>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>          
+            <h1>E-Mail: {user.email}</h1>
+            <Image
+                style={{width: 50, height: 50}}
+                source={{uri : user.url}}
+            />
+          </div>
+          );
+      })}  
+    </div>
   );
 }
 
